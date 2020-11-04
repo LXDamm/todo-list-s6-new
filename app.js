2e2f1ceded456b9c99a96747828d7e8be6fb0219
Lexx Utopia Olivia Damm <lexx.damm@gmail.com>
2020-11-04 15:25:26 +0100

    Copy from first repo

---
+++ app.js
@@ -0,0 +1,33 @@
+let todoList = [];
+
+todoList.push({id: 0, title: 'Test', eventDate: new Date(), description: 'Some description text'});
+todoList.push({id: 1, title: 'Works', eventDate: new Date(), description: 'This works too!'});
+
+const todoElm = document.querySelector('.todo-list');
+
+function populateTodoItem(item) {
+    let iconHTML = '';
+    if (item.checked) {
+        iconHTML = '<i class="fas fa-check"></i>';
+    }
+
+    return `<li><div><span>Title: ${item.title}</span> <span>${iconHTML} <span>${item.eventDate.toUTCString()}</span><br>${item.description}<div></li>`;
+}
+
+function populateList() {
+    todoElm.innerHTML = '';
+    todoList.forEach((item) => {
+        todoElm.innerHTML += populateTodoItem(item);
+    });
+}
+
+function populateUI() {
+    populateList();
+}
+
+function loadEventListeners() {
+    addButtonElm.addEventListener('click', addTodoItem);
+    document.addEventListener('DOMContentLoaded', populateUI);
+}
+
+loadEventListeners();
